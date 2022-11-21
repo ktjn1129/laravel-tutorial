2a14f6382f2d1642da40ca2a5169dc0bc71a98e7
koto <koto@aivick.co.jp>
2022-11-21 13:15:39 +0900

    ユニットテストの実施

---
+++ src/database/factories/PersonFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ */
+class PersonFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => fake()->name(),
+            'mail' => fake()->unique()->safeEmail(),
+            'age' => random_int(1,99),
+        ];
+    }
+}
